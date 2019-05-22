db7d7b6752fabdb7ddd38cf8542c64ea509f9131
Michael <michaelkallies88@gmail.com>
2019-05-21 22:49:22 -0400

    wip: outline for presentation

---
+++ button.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+import { color, space, fontSize } from "styled-system"
+
+const Button = styled.button`
+  ${color}
+  ${space}
+  ${fontSize}
+`
+
+export default Button
